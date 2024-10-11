f1ec75882b500c734d72ce137d8e656b448818cd
murasaki718 <28164792+murasaki718@users.noreply.github.com>
2024-10-10 19:19:53 -0500

    replaced with ec2 module - maybe

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,7 +1,6 @@
-output "controller_public_ip" {
-  value = aws_instance.controller.public_ip
-}
-
-output "worker_node_public_ip" {
-  value = aws_instance.nodes[*].public_ip
+output "nodes_public_ip" {
+  value = {
+    Controller = try(module.controller.public_ip, null)
+    Workers = try(module.worker_nodes[*].public_ip, null)
+  }
 }
