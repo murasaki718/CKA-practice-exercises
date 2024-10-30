3b9df0cd5ed59ab75c53ee3d444823040befab3f
murasaki718 <28164792+murasaki718@users.noreply.github.com>
2024-10-30 15:29:52 -0500

    WIP

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -1,3 +1,6 @@
+variable "aws_region" {
+  type = string
+}
 variable "key_pair_name" {
   type    = string
   default = "k8s"
@@ -6,7 +9,9 @@ variable "public_key_path" {
   type    = string
   default = "~/.ssh/id_rsa.pub"
 }
+variable "controller_node_count" {
+  type = number
+}
 variable "worker_node_count" {
   type = number
-  default = 1
 }
